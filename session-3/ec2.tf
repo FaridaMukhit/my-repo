fa8353340c7c44fd7dc8110b30d0e1fb112f6c52
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-06 20:05:58 -0600

    sg

--- session-3/ec2.tf
+++ session-3/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "first_ec2" {
   ami           = "ami-0aa7d40eeae50c9a9"
+  vpc_security_group_ids = [ aws_security_group.web5.id ]
   instance_type = "t2.micro"
   tags = {
     Name        = "first"
