ebdc7f26e3f7c98dc91d922c00bfb43c0b3dfb51
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-08 21:05:57 -0600

    format function

--- session-4/variables.tf
+++ session-4/variables.tf
@@ -9,9 +9,3 @@ variable "instance_type" {
     description = "this is a variable for instance type"
     default = "t2.micro"
 }
-
-variable "ami" {
-    type = string
-    description = "this is a variable for ami id"
-    default = "ami-026b57f3c383c2eec"
-}
