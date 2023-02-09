cc8f11d6403628746b2a17dcd5d01f9659cdbde5
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-08 19:43:56 -0600

    added data source

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "main" {
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ aws_security_group.web.id ]
   tags = {
     Name        = var.env
   }
