c96b24a7815df9bd382648a05a89c466c493f67d
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-08 19:40:09 -0600

    added data source

--- session-4/variables.tf
+++ session-4/variables.tf
@@ -10,44 +10,8 @@ variable "instance_type" {
     default = "t2.micro"
 }
 
-variable "main_vpc_cidr" {
+variable "ami" {
     type = string
-    description = "This is a variable for the Main Vpc Cidr Block"
-    default = "10.0.0.0/16"
-}
-
-variable "public_subnet1_cidr" {
-    type = string
-    description = "This is a variable for the Public Subnet-1 Cidr Block"
-    default = "10.0.1.0/24"
-}
-
-variable "public_subnet2_cidr" {
-    type = string
-    description = "This is a variable for the Public Subnet-2 Cidr Block"
-    default = "10.0.2.0/24"
-}
-
-variable "public_subnet3_cidr" {
-    type = string
-    description = "This is a variable for the Public Subnet-3 Cidr Block"
-    default = "10.0.3.0/24"
-}
-
-variable "private_subnet1_cidr" {
-    type = string
-    description = "This is a variable for the Private Subnet-1 Cidr Block"
-    default = "10.0.11.0/24"
-}
-
-variable "private_subnet2_cidr" {
-    type = string
-    description = "This is a variable for the Private Subnet-2 Cidr Block"
-    default = "10.0.12.0/24"
-}
-
-variable "private_subnet3_cidr" {
-    type = string
-    description = "This is a variable for the Private Subnet-3 Cidr Block"
-    default = "10.0.13.0/24"
+    description = "this is a variable for ami id"
+    default = "ami-026b57f3c383c2eec"
 }
