87a48f40b61bc123b742cd8b038905141d5b7434
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-11 12:25:07 -0600

    added count

--- session-5/ec2.tf
+++ session-5/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "main" {
+  count = 3
   ami           = data.aws_ami.amazon_linux_2.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.web1.id ]
