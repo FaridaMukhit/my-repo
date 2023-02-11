b22ae3b26292d2bfaf541954d0ff8bbc88772b94
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-11 12:38:32 -0600

    added element

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -2,3 +2,20 @@ resource "aws_security_group" "web1" {
   name        = "web-instance-sg"
   description = "this is a security group for web instance"
 }
+resource "aws_security_group_rule" "web_ingress" {
+  type              = "ingress"
+  from_port         = element(var.ports, 0)
+  to_port           = element(var.ports, 0)
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web1.id
+}
+
+resource "aws_security_group_rule" "web_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.web1.id
+}
