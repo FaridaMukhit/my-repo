e7eda307580c67e040ce9f1c474283a149a394f2
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-11 11:35:29 -0600

    added state lock

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "farida-terraform-februray"
     region         = "us-east-1"
     key            = "session-5/terraform.tfstate"
-    dynamodb_table = "terraform-may2022-state-lock-table"
+    dynamodb_table = "terraform-februry-state-lock"
   }
 }
