1bc3c6a6e599ff00e0a7d41533f86d1b545e9cab
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-13 20:00:33 -0600

    script

---
+++ session-6/Filestructure/terraform.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+rm -rf .terraform
+read -p "which environment would you like?"
+echo $ENV 
+sed -i "s/_env_/$ENV/g" backend.tf
+echo "Environment is set ot $ENV"
+
+terraform init
+terraform plan -var-files-$ENV.tfvars
+
+read -p "are you sure you want to apply it?" apply
+if $apply == [yes]:
+then terrafrom apply -var-files-$ENV.tfvars -auto-approve
+else echo "terraform is not gonna apply"
+fi
+
+echo "rolling back the backend"
+sed -i "s/$ENV/_env_/g" backend.tf
+echo "script ran successfully"
+
+// 1. updating backend file
+// 2. terraform init
+// 3. terrafrom plan -var-files.tfvars
+// 4. terraform apply
