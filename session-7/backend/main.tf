9b40a51b9f2982ce921546896369f0ca657827d6
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-15 21:13:41 -0600

    conditions

--- session-7/backend/main.tf
+++ session-7/backend/main.tf
@@ -1,4 +1,20 @@
-resource "aws_sqs_queue" "main" {
-name = replace(local.name, "resource", main-sqs)
-tags = local.common_tags
+resource "aws_db_instance" "main" {
+  allocated_storage    = 10
+  identifier = replace(local.name, "resource", "rds")
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = random_password.db_password.result
+  skip_final_snapshot  = var.env != "prod" ? true : false
+  final_snapshot_identifier = var.env != "prod" ? null : replace(local.name, "resource", "db_final_snapshot")
 }
+
+resource "random_password" "db_password" {
+    length = 20
+    special = true
+    override_special = "@%"
+}
+
+
