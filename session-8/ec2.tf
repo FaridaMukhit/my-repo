8c94c946d99953beccfe2dd9684499c250f42967
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-20 19:08:31 -0600

    added

--- session-8/ec2.tf
+++ session-8/ec2.tf
@@ -3,8 +3,15 @@ resource "aws_instance" "main" {
   instance_type = var.instance_type
   vpc_security_group_ids = [ aws_security_group.web1.id ]
   key_name = aws_key_pair.teraform_server.key_name
-  tags = {
-    Name        = "${var.env}-instance"
+  provisioner "file" {
+    source = "~/my-repo/session-8/index.html"
+    destination = "/tmp/index.html"
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      host = self.public_ip
+      private_key = file("~/.ssh/id_rsa")
+    }
   }
 }
 resource "aws_key_pair" "teraform_server" {
