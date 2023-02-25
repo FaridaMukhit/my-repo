c086013076546eea3a09973215669c46b4637ee7
FaridaMukhit <mukhitfarida@gmail.com>
2023-02-25 12:42:04 -0600

    added

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-    source = "github.com/FaridaMukhit/my-repo/modules/ec2.git?ref=v1.0.0"
+    source = "github.com/FaridaMukhit/my-repo//modules/ec2?ref=v1.0.0"
     env = "dev"
     ami = "ami-0aa7d40eeae50c9a9"
     instance_type = "t2.micro"
